7c89d44e29e7c48bcb196002af5a1a747656d18b
MarcusMJV <vorstermarcus@gmail.com>
2026-03-18 22:12:24 +0200

    only load dev certs in vite config if they exist

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-import { readFileSync } from 'node:fs'
+import { existsSync, readFileSync } from 'node:fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+const hasCerts = existsSync('./certs/localhost+2-key.pem')
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    https: {
+    https: hasCerts ? {
       key: readFileSync('./certs/localhost+2-key.pem'),
       cert: readFileSync('./certs/localhost+2.pem'),
-    },
+    } : undefined,
     allowedHosts: true,
   },
   plugins: [
